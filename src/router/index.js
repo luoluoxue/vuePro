fa92f39ff1b7b01c02e7dc6bb47d5cd85631b1d8
lcx <473887719@qq.com>
2020-12-23 15:14:36 +0800

    使用路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import index from "../view/index/index.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const routes = [
+  {
+    path: "/",
+    name: "index",
+    component: index
+  },
+  {
+    path: "/main",
+    name: "main",
+    component: () => import("../view/main/main.vue")
+  }
+];
+const router = new Router({
+  routes
+});
+export default router;
